b2599807df056d4fba5953db5a2cbbedde13701d
nonamich <dimaeromin97@gmail.com>
2025-03-13 19:12:11 +0200

    add email verification functionality with SymfonyCasts VerifyEmailBundle

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
