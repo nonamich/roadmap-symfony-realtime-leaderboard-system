602c8a5a1713a2bf6e123784fc5af259946ca2e8
nonamich <dimaeromin97@gmail.com>
2025-03-28 02:38:54 +0200

    refactor: remove commented-out code in MovieRepository and ShowRepository for improved readability

--- src/Repository/ShowRepository.php
+++ src/Repository/ShowRepository.php
@@ -7,7 +7,7 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends ServiceEntityRepository<Show>
+ * @extends ServiceEntityRepository<MovieShow>
  */
 class ShowRepository extends ServiceEntityRepository
 {
@@ -15,29 +15,4 @@ class ShowRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, MovieShow::class);
     }
-
-    //    /**
-    //     * @return Show[] Returns an array of Show objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('s')
-    //            ->andWhere('s.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('s.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Show
-    //    {
-    //        return $this->createQueryBuilder('s')
-    //            ->andWhere('s.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
